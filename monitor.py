5ad7ec449c0734c13849b0055221c2419cf97293
Sudeep <sudeep.rp@gmail.com>
2025-08-12 14:48:02 +0530

    range abstraction

--- monitor.py
+++ monitor.py
@@ -14,22 +14,26 @@ def displayAlertMessage(message):
     sleep(1)
 
 
+def is_in_range(value, min_value, max_value):
+  return min_value <= value <= max_value
+
+
 def temperature_ok(temperature):
-  if temperature > 102 or temperature < 95:
+  if not is_in_range(temperature, 95, 102):
     displayAlertMessage('Temperature critical!')
     return False
   return True
 
 
 def pulse_rate_ok(pulseRate):
-  if pulseRate < 60 or pulseRate > 100:
+  if not is_in_range(pulseRate, 60, 100):
     displayAlertMessage('Pulse Rate is out of range!')
     return False
   return True 
 
 
 def spo2_ok(spo2):
-  if spo2 < 90:
+  if not is_in_range(spo2, 90, 100):
     displayAlertMessage('Oxygen Saturation out of range!')
     return False
   return True
