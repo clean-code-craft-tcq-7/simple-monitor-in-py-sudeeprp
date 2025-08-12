60058f5b6c48b29a38f7873e8bcecc211c006684
Sudeep <sudeep.rp@gmail.com>
2025-08-12 16:55:30 +0530

    add blood sugar in the new interface

--- test_monitor.py
+++ test_monitor.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from monitor import vitals_ok, alert_if_not_in_range
+from monitor import vitals_ok, alert_if_not_in_range, report_is_normal
 
 
 class MonitorTest(unittest.TestCase):
